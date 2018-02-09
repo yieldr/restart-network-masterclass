f4692eec77b9188809a64d11bd59fa75552278a8
hangyu <hang.yu@yieldr.com>
2018-02-09 14:31:19 +0100

    Add pagination

--- src/AppBundle/Entity/Booking.php
+++ src/AppBundle/Entity/Booking.php
@@ -4,6 +4,7 @@ namespace AppBundle\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * A flight.
